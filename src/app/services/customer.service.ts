f1bc5c89559b5cb77985b3f62330cfd51b41d5d7
Christopher Dent <christopherdent01@gmail.com>
2021-09-26 13:53:24 -0400

    resolve cors issue

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseURL = 'https://treehousechallenge.contractornation.com/';
+const baseURL = 'https://treehousechallenge.contractornation.com';
 
 
 @Injectable({

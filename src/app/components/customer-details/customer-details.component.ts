16a89677b02301d2b7d34f157dd34e9a83a93818
Christopher Dent <christopherdent01@gmail.com>
2021-09-26 13:16:03 -0400

    add custome list

--- src/app/components/customer-details/customer-details.component.ts
+++ src/app/components/customer-details/customer-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from 'src/app/services/customer.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-details',
@@ -6,10 +8,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-details.component.css']
 })
 export class CustomerDetailsComponent implements OnInit {
+  currentCustomer = null;
+  message = '';
 
-  constructor() { }
+  constructor(
+    private customerService: CustomerService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+   
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+      this.message = '';
+      this.getCustomer(this.route.snapshot.paramMap.get('id'));
+    }
+
+
+    getCustomer(id): void {
+      this.customerService.read(id)
+        .subscribe(
+          customer => {
+            this.currentCustomer = customer;
+            console.log(customer);
+          },
+          error => {
+            console.log(error);
+          });
+    }
+  
+    setAvailableStatus(status): void {
+      const data = {
+        name: this.currentCustomer.name,
+        description: this.currentCustomer.description,
+        available: status
+      };
+  
+      this.customerService.update(this.currentCustomer.id, data)
+        .subscribe(
+          response => {
+            this.currentCustomer.available = status;
+            console.log(response);
+          },
+          error => {
+            console.log(error);
+          });
+    }
+  
+    updateCustomer(): void {
+      this.customerService.update(this.currentCustomer.id, this.currentCustomer)
+        .subscribe(
+          response => {
+            console.log(response);
+            this.message = 'The customer was updated!';
+          },
+          error => {
+            console.log(error);
+          });
+    }
+  
+    deleteCustomer(): void {
+      this.customerService.delete(this.currentCustomer.id)
+        .subscribe(
+          response => {
+            console.log(response);
+            this.router.navigate(['/customers']);
+          },
+          error => {
+            console.log(error);
+          });
+    }
+
+
+
+
+
 
 }
